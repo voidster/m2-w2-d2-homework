1daae2f00a0f2378add16ed20d6d2c04d7f31be7
voidster <eszettv@icloud.com>
2025-02-27 15:36:40 -0800

    all done

--- js/activity.js
+++ js/activity.js
@@ -1,32 +1,28 @@
-// User interaction with table cells
 $(document).ready(function () {
+  let selectedActivities = new Set(); // Store selected activities uniquely
+
   $("td:not(:first-child)").click(function () {
     var content = $(this).text(); // Get the activity name
-
     if (content !== "Not Available") {
       $(this).toggleClass("tdhighlight"); // Highlight selection
 
-      // Get the column index of the selected cell
       var columnIndex = $(this).index();
-
-      // Get the corresponding cliff name from the table header
       var cliffName = $("thead th").eq(columnIndex).text();
-
-      // Create the final display text
       var displayText = content + " at " + cliffName;
 
       if ($(this).hasClass("tdhighlight")) {
-        $("#displaySelected").css("visibility", "visible");
-        $("#displaySelected").css("margin-top", "2em");
-        $("#result").append("<p>" + displayText + "</p>");
+        selectedActivities.add(displayText); // Add activity
       } else {
-        $("#result p:contains('" + displayText + "')").remove();
-
-        if ($("#result").has("p").length == false) {
-          $("#displaySelected").css("visibility", "hidden");
-          $("#displaySelected").css("margin-top", "0");
-        }
+        selectedActivities.delete(displayText); // Remove if deselected
       }
+
+      // Update modal content
+      $("#modalBody").html(
+        [...selectedActivities].map((item) => `<p>${item}</p>`).join("")
+      );
+
+      // Show the modal
+      $("#activityModal").modal("show");
     }
   });
 });
